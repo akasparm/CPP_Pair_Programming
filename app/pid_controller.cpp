386119a3c01f47173c1402de32756411cd122db6
SuryaVC <chappidi@umd.edu>
2023-10-10 14:58:57 +0000

    implemented the pid controller

--- app/pid_controller.cpp
+++ app/pid_controller.cpp
@@ -4,5 +4,20 @@
 #include <iostream>
 
 double PIDController::compute(double setpoint, double actual_velocity) {
-  return 0.0;
+
+  // calculating the error
+  double error = setpoint - actual_velocity;
+
+  // calculating the derivative error
+  double derivative_error = (error - previous_error)/dt;
+
+  // calculating the integral error
+  double integral_error = error * dt;
+
+  // calculating the total output 
+  double output = kp * error + kd * derivative_error + ki * integral_error;
+
+  previous_error = error;
+
+  return output;
 }
